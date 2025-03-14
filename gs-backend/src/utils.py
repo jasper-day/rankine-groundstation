2deb8ada785cc47097137d918c7acf33c0f3d601
olli <65198941+knokbak@users.noreply.github.com>
2025-03-14 01:54:23 +0000

    chore(backend): fancy license notice

--- gs-backend/src/utils.py
+++ gs-backend/src/utils.py
@@ -13,5 +13,12 @@ def print_super_amazing_ascii_text_banner() -> None:
 
 def print_start_notice() -> None:
     print("Endeavour - Rankine EvenFlow Ground Station Backend\n")
-    print("This is free and unencumbered software released into the public domain.")
-    print("Subject to the terms and conditions of the Unlicense: https://unlicense.org\n")
+    print("Copyright (C) Ollie Killean, James Kitching, Jasper Day - 2025\n")
+    print("This program is free software; you can redistribute it and/or")
+    print("modify it under the terms of the GNU Lesser General Public")
+    print("License as published by the Free Software Foundation; either")
+    print("version 3 of the License, or (at your option) any later version.\n")
+    print("This program is distributed in the hope that it will be useful,")
+    print("but WITHOUT ANY WARRANTY; without even the implied warranty of")
+    print("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU")
+    print("Lesser General Public License for more details.\n")
