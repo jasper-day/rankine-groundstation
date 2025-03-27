d64e6e9ee91482c643a6e55da5a074859616951b
Jasper Day <jasper@linearized.xyz>
2025-03-27 18:57:23 +0000

    backend: Add error handling to protocol

--- gs-backend/src/protocol_path.py
+++ gs-backend/src/protocol_path.py
@@ -1,5 +1,6 @@
 from packet import Packet
 from path import path_adapter, solve
+from pydantic import ValidationError
 
 async def protocol_path(packet: Packet) -> None:
     id, method, replying_to, data = packet.id, packet.method, packet.replying_to, packet.data
@@ -8,7 +9,10 @@ async def protocol_path(packet: Packet) -> None:
         case "ping":
             await packet.reply("path:pong")
         case "solve":
-            path = path_adapter.validate_python(data['path'])
+            try:
+                path = path_adapter.validate_python(data['path'])
+            except ValidationError as e:
+                await packet.error(e)
             await packet.reply("path:solved", data={
                 "path": path_adapter.dump_python(solve(path))
             })
